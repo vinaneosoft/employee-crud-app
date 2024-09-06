63f162f24da4b9b99b0c5677d18eaa0445578d57
vinaneosoft <vinaneosoft@gmail.com>
2024-09-06 12:54:40 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent   // components, pipes
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+   // components, pipes
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule    // other modules
+    AppRoutingModule // other modules
   ],
   providers: [],        // services
   bootstrap: [AppComponent]  // root component
