e935eb4d1b4f0f75e83ebc06c18069b3a4f674dd
vinaneosoft <vinaneosoft@gmail.com>
2024-09-10 12:26:08 +0530

    commit

--- src/app/learning/learning.component.ts
+++ src/app/learning/learning.component.ts
@@ -17,7 +17,28 @@ export class LearningComponent implements OnInit, OnDestroy, OnChanges {
   className=LearningComponent;
 
    companyOffices=['Dadar, Mumbai', 'Parel, mumbai','Rabale, mumbai', 'pune', 'noida', 'Indore']
-
+  offices=[
+    {
+      location:'Dadar, Mumbai (Headquarters)',
+      address:'The Ruby Tower, Senapati Bapat Marg, Dadar West, Mumbai, Maharashtra 400028'
+    },
+    {
+      location:'Parel, Mumbai',
+      address:'Business Arcade, Sayani Road, Opp. S. T Bus Stand, Lower Parel, Mumbai, Maharashtra 400013'
+    },
+    {
+      location:'Rabale, Mumbai',
+      address:'Sigma IT Park, Unit No. 501, TTC Industrial Area, Rabale, Navi Mumbai, Maharashtra 400701'
+    },
+    {
+      location:'Pune',
+      address:'IT-09, 10th Floor, Blue Ridge SEZ, Hinjewadi, Pune, Maharashtra 411057'
+    },
+    {
+      location:'Noida',
+      address:'WorkEdge Coworx - Coworking Noida, B 23, Sector 63 Road, B Block, Sector 63, Noida, Uttar Pradesh 201301'
+    }
+  ]
   constructor(){
     console.log("in learning constrcutor");
     setInterval(()=>{
