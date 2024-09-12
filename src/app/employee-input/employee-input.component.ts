8583b144432151e6f71d95bafe45899a4773ddea
vinaneosoft <vinaneosoft@gmail.com>
2024-09-12 15:09:35 +0530

    commit

--- src/app/employee-input/employee-input.component.ts
+++ src/app/employee-input/employee-input.component.ts
@@ -25,12 +25,38 @@ export class EmployeeInputComponent {
       c_secrete_code:new FormControl()
     });
   }
-
+/* u can keep getter name and FormControl same or different */
+  get _id(){
+    return this.employeeForm.get('_id'); // returing FormControl object
+  }
+  get emp_name(){
+    return this.employeeForm.get('emp_name'); // returing FormControl object
+  }
+  get joining_date(){
+    return this.employeeForm.get('joining_date'); // returing FormControl object
+  }
+  get experience(){
+    return this.employeeForm.get('experience'); // returing FormControl object
+  }
+  get emp_email(){
+    return this.employeeForm.get('emp_email'); // returing FormControl object
+  }
+  get dept_code(){
+    return this.employeeForm.get('dept_code'); // returing FormControl object
+  }
+  get secrete_code(){
+    return this.employeeForm.get('secrete_code'); // returing FormControl object
+  }  
+  get c_secrete_code(){
+    return this.employeeForm.get('c_secrete_code'); // returing FormControl object
+  }
   collectData(){
     console.log(this.employeeForm);
     console.log(this.employeeForm.value);
     //i want data of only id
    // console.log(this.employeeForm.value._id);
    //i want data of only id via FormControl
+   //console.log(this.employeeForm.controls._id.value.controls._id.value);
+   console.log(this.empId?.value);
   }
 }
